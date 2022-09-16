a1e39cc6b4ec4b0944a45c20747b087138df592e
William <william.gruber@live.de>
2022-09-16 19:03:51 +0200

    Project done

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello World!");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4"); // STRG + D kopierte neue Zeile
+    }
+
+}
